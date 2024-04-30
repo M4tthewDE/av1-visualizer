1d9bc8d4e665a671260cdda1629cb692283e6b4f
M4tthewDE <matthias.kronberg@gmail.com>
2024-04-30 20:32:11 +0200

    feat: parse stts

--- src/decoder/mp4/stbl.rs
+++ src/decoder/mp4/stbl.rs
@@ -1,19 +1,69 @@
 use anyhow::{bail, Context};
 use std::io::{Cursor, Read};
+use tracing::info;
 
 use anyhow::Result;
 
 use super::stsd::Stsd;
 
+pub type SampleEntry = (u32, u32);
+
+#[derive(Clone, Debug, Default)]
+pub struct Stts {
+    pub version: u8,
+    pub flags: [u8; 3],
+    pub entries: Vec<SampleEntry>,
+}
+
+impl Stts {
+    #[tracing::instrument(skip_all, name = "stbl")]
+    pub fn new(c: &mut Cursor<Vec<u8>>) -> Result<Stts> {
+        let mut version = [0u8; 1];
+        c.read_exact(&mut version)?;
+
+        let mut flags = [0u8; 3];
+        c.read_exact(&mut flags)?;
+
+        let mut entry_count = [0u8; 4];
+        c.read_exact(&mut entry_count)?;
+        let entry_count = u32::from_be_bytes(entry_count);
+
+        let mut entries = Vec::new();
+        for _ in 0..entry_count {
+            let mut sample_count = [0u8; 4];
+            c.read_exact(&mut sample_count)?;
+            let sample_count = u32::from_be_bytes(sample_count);
+
+            let mut sample_delta = [0u8; 4];
+            c.read_exact(&mut sample_delta)?;
+            let sample_delta = u32::from_be_bytes(sample_delta);
+
+            entries.push((sample_count, sample_delta));
+        }
+
+        let stts = Stts {
+            version: version[0],
+            flags,
+            entries,
+        };
+
+        info!("stts: {stts:?}");
+
+        Ok(stts)
+    }
+}
+
 #[derive(Clone, Debug, Default)]
 pub struct Stbl {
     pub stsd: Stsd,
+    pub stts: Stts,
 }
 
 impl Stbl {
     #[tracing::instrument(skip_all, name = "stbl")]
     pub fn new(c: &mut Cursor<Vec<u8>>, size: usize) -> Result<Stbl> {
         let mut stsd = None;
+        let mut stts = None;
         loop {
             let mut box_size = [0u8; 4];
             c.read_exact(&mut box_size)?;
@@ -25,6 +75,7 @@ impl Stbl {
 
             match box_type.as_str() {
                 "stsd" => stsd = Some(Stsd::new(c)?),
+                "stts" => stts = Some(Stts::new(c)?),
                 typ => bail!("box type {typ:?} not implemented"),
             }
 
@@ -33,8 +84,13 @@ impl Stbl {
             }
         }
 
-        Ok(Stbl {
+        let stbl = Stbl {
             stsd: stsd.context("no stsd found")?,
-        })
+            stts: stts.context("no stts found")?,
+        };
+
+        info!("stbl: {stbl:?}");
+
+        Ok(stbl)
     }
 }
